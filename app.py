b217bc5fb5ce4e2819b85ff2081eeac13b1137e1
Blair Huang <qqnine5156@gmail.com>
2022-04-19 17:51:22 +0800

    try again

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = flask.Flask(__name__)
 def index():
     start_coords = (23.000373101863882, 120.18625642830294)
     folium_map = folium.Map(location=start_coords, zoom_start=14)
-    return "folium_map._repr_html_()"
+    return folium_map._repr_html_()
 
 if __name__ == '__main__':
     app.run()
