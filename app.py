7f47f73b1fb6f8e2650eef24ab3e5ba89f43c7db
Blair Huang <qqnine5156@gmail.com>
2022-04-19 17:48:32 +0800

    folium

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import flask
+import folium
+
 app = flask.Flask(__name__)
 
 @app.route("/")
-@app.route("/hello")
-def hello():
-    return "Hello, World!"
+def index():
+    start_coords = (23.000373101863882, 120.18625642830294)
+    folium_map = folium.Map(location=start_coords, zoom_start=14)
+    return "folium_map._repr_html_()"
 
 if __name__ == '__main__':
     app.run()
